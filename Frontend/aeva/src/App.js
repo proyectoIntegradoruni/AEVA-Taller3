759dbb6edb753815e8796615ec7c46998cbff11d
Marcela <rivas.lina@correounivalle.edu.co>
2024-04-07 17:12:19 -0500

    implementacion del diseño de la interfaz de inicio

---
+++ Frontend/aeva/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Inicio from './Inicio';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Inicio/>
+    </div>
+  );
+}
+
+export default App;
