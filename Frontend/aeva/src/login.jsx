ce4a67c0066df8a2b617624622e91ba032520e56
KPaola-M74 <kampao74@gmail.com>
2024-04-23 12:50:05 -0500

    login conexion

--- Frontend/aeva/src/login.jsx
+++ Frontend/aeva/src/login.jsx
@@ -17,7 +17,7 @@ function Login() {
     e.preventDefault();
   
     try {
-      const response = await fetch('http://localhost:3001/login', {
+      const response = await fetch('https://tarea-2.onrender.com/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
